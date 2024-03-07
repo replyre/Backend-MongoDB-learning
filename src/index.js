b620e38c39af61ba1f6c0d4b426971669fcfb642
replyre <121796450+replyre@users.noreply.github.com>
2024-03-07 23:29:41 +0530

    api error handling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+import { app } from "./app.js";
 import connectDb from "./db/index.js";
 import dotenv from "dotenv";
 
 dotenv.config({
   path: "./env",
 });
-connectDb();
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is working ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch(() => {
+    console.log("Mongo DB connect ");
+  });
 // (async () => {
 //   try {
 //     await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`);
