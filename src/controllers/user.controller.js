518968ed9cf440da7839855dee00941a5ebb1ac5
replyre <121796450+replyre@users.noreply.github.com>
2024-03-22 18:47:55 +0530

    controller build

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.model.js";
 import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
@@ -13,18 +14,24 @@ const registerUser = asyncHandler(async (req, res) => {
   ) {
     throw new ApiError(400, "All fileds are required");
   }
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
   if (existedUser) throw new ApiError(409, "USer alrerady exists");
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files?.coverImage[0]?.path;
+  }
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path || "";
 
   if (!avatarLocalPath) throw new ApiError(400, "avatar file not found");
-  if (!coverImageLocalPath)
-    throw new ApiError(400, "coverImage file not found");
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
@@ -35,7 +42,7 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({
     fullname,
     avatar: avatar.url,
-    coverImage: coverImage,
+    coverImage: coverImage?.url || " ",
     email,
     password,
     username: username.toLowerCase(),
@@ -47,6 +54,10 @@ const registerUser = asyncHandler(async (req, res) => {
   if (!createdUser) {
     throw new ApiError(500, "Something went wrong");
   }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
 
 export { registerUser };
--- src/db/index.js
+++ src/db/index.js
@@ -5,8 +5,9 @@ import { json } from "express";
 const connectDb = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGO_URL}/${DB_NAME}`
+      `${process.env.MONGO_URL}${DB_NAME}`
     );
+    console.log(`${process.env.MONGO_URL}${DB_NAME}`);
     console.log(
       `MongoDB Connected ! Host ${connectionInstance.connection.host}`
     );
